52b2f30505b1925dc7f7fe650bc90bf0d91e70d5
viktoriazakatsyura <viktoriazakatsyura@gmail.com>
2023-04-09 18:51:07 +0300

    sd

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line react-hooks/exhaustive-deps
+
 
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
@@ -74,6 +74,7 @@ const ImageGallery = ({ queriesName, handleImageClick }) => {
         onFetchQuery();
       }, 500);
     }
+     // eslint-disable-next-line
   }, [queriesName]);
 
   return (
