2cb9bcef7ae99598a8301f222e34953e28a72af1
viktoriazakatsyura <viktoriazakatsyura@gmail.com>
2023-04-09 14:26:22 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
@@ -9,56 +9,52 @@ import Modal from './components/Modal/Modal';
 import ImageGallery from './components/ImageGallery/ImageGallery';
 
 
-class App extends Component{
-  state = {
-    isShow: false,
-    loader: false,
-    queriesName: '',
-    largeImageURL: '',
-    imgTags: '',
-  }
+function App() {
+  const [isShow, setIsShow] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const [queriesName, setQueriesName] = useState('');
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [imgTags, setImgTags] = useState('');
 
-  toggleModal=()=>{
-    this.setState(({isShow})=>({isShow: !isShow}))
-  }
+  const toggleModal = () => {
+    setIsShow(isShow => !isShow);
+  };
 
-  handleFormSubmit = (queriesName) => {
-    this.setState({ queriesName })
-  }
+  const handleFormSubmit = (queriesName) => {
+    setQueriesName(queriesName);
+  };
 
+  const handleImageClick = (largeImageURL, imgTags) => {
+    setLargeImageURL(largeImageURL);
+    setImgTags(imgTags);
+    setLoader(true);
+    toggleModal();
+  };
 
-  handleImageClick=(largeImageURL, imgTags)=>{
-    this.setState({largeImageURL, imgTags, loader: true})
-    this.toggleModal()
-  }
+  const hideModalLoader = () => {
+    setLoader(false);
+  };
 
-  hideModalLoader=()=> this.setState({loader: false})
-
-  render() {
-     const { isShow, queriesName , largeImageURL, imgTags, loader } = this.state
-    return (
-      <>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-
-        <ToastContainer autoClose={4000} />
-        <ImageGallery
-          queriesName={queriesName}
-          handleImageClick={this.handleImageClick}
-        />
-
-        {isShow && (
-          <Modal onClose={this.toggleModal}>
-            {loader && <ModalLoader />}
-            <img
-              src={largeImageURL}
-              alt={imgTags}
-              onLoad={this.hideModalLoader}
-            />
-          </Modal>
-        )}
-      </>
-    )
-  }
+  return (
+    <>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ToastContainer autoClose={4000} />
+      <ImageGallery
+        queriesName={queriesName}
+        handleImageClick={handleImageClick}
+      />
+      {isShow && (
+        <Modal onClose={toggleModal}>
+          {loader && <ModalLoader />}
+          <img
+            src={largeImageURL}
+            alt={imgTags}
+            onLoad={hideModalLoader}
+          />
+        </Modal>
+      )}
+    </>
+  );
 }
 
 App.propTypes = {
@@ -68,3 +64,75 @@ App.propTypes = {
 }
 
 export default App;
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css'
+// import { ModalLoader } from './components/Loader/Loader';
+// import PropTypes from 'prop-types'
+// import Searchbar from './components/SeacrhBar/Searchbar';
+// import Modal from './components/Modal/Modal';
+// import ImageGallery from './components/ImageGallery/ImageGallery';
+
+
+// class App extends Component{
+//   state = {
+//     isShow: false,
+//     loader: false,
+//     queriesName: '',
+//     largeImageURL: '',
+//     imgTags: '',
+//   }
+
+//   toggleModal=()=>{
+//     this.setState(({isShow})=>({isShow: !isShow}))
+//   }
+
+//   handleFormSubmit = (queriesName) => {
+//     this.setState({ queriesName })
+//   }
+
+
+//   handleImageClick=(largeImageURL, imgTags)=>{
+//     this.setState({largeImageURL, imgTags, loader: true})
+//     this.toggleModal()
+//   }
+
+//   hideModalLoader=()=> this.setState({loader: false})
+
+//   render() {
+//      const { isShow, queriesName , largeImageURL, imgTags, loader } = this.state
+//     return (
+//       <>
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+
+//         <ToastContainer autoClose={4000} />
+//         <ImageGallery
+//           queriesName={queriesName}
+//           handleImageClick={this.handleImageClick}
+//         />
+
+//         {isShow && (
+//           <Modal onClose={this.toggleModal}>
+//             {loader && <ModalLoader />}
+//             <img
+//               src={largeImageURL}
+//               alt={imgTags}
+//               onLoad={this.hideModalLoader}
+//             />
+//           </Modal>
+//         )}
+//       </>
+//     )
+//   }
+// }
+
+// App.propTypes = {
+//   isShow: PropTypes.bool,
+//   queriesName: PropTypes.string,
+//   loader: PropTypes.bool,
+// }
+
+// export default App;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,137 +1,257 @@
-
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import s from './ImageGallery.module.css';
-import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
-import Button from '../Button/Button';
-import {GalleryLoader } from '../Loader/Loader';
-import { fetchQuery } from '../Api/Api';
-import { onErrorToast} from '../ErorrTost/ErorrTost';
-import { onErrorToastNoMore} from '../ErorrTost/ErorrTost';
-
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import s from "./ImageGallery.module.css";
+import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
+import Button from "../Button/Button";
+import { GalleryLoader } from "../Loader/Loader";
+import { fetchQuery } from "../Api/Api";
+import { onErrorToast } from "../ErorrTost/ErorrTost";
+import { onErrorToastNoMore } from "../ErorrTost/ErorrTost";
 
 const Status = {
-  IDLE: 'idle',
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
+  IDLE: "idle",
+  PENDING: "pending",
+  RESOLVED: "resolved",
+  REJECTED: "rejected",
 };
 
-class ImageGallery extends Component {
-  state = {
-    queries: [],
-    error: null,
-    status: Status.IDLE,
-    page: 1,
-    hasMore: true
-  }
+function ImageGallery({ queriesName, handleImageClick }) {
+  const [queries, setQueries] = useState([]);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
-  onLoadMoreBtn = (e) => {
-    e.preventDefault();
-    setTimeout(() => {
-      this.onFetchQuery();
-    }, 500);
-    this.scrollPageToEnd()
-  }
+  useEffect(() => {
+    if (queriesName !== "") {
+      setStatus(Status.PENDING);
+      setPage(1);
+      setQueries([]);
+      setHasMore(true);
+      fetchQueries();
+    }
+  }, [queriesName]);
 
-  onFetchQuery() {
-    const searchQuery = this.props.queriesName;
-
-    const { page } = this.state;
-    fetchQuery(searchQuery, page)
-      .then((queries) => {
-        if (queries.hits.length === 0) {
-          this.setState({ hasMore: false }, () => {
-            onErrorToast();
-          });
+  function fetchQueries() {
+    fetchQuery(queriesName, page)
+      .then((response) => {
+        const newQueries = response.hits;
+        if (newQueries.length === 0) {
+          setHasMore(false);
+          onErrorToast();
         } else {
-          this.setState((prevState) => ({
-            queries: [...prevState.queries, ...queries.hits],
-            page: this.state.page + 1,
-            status: Status.RESOLVED,
-          }));
+          setQueries((prevQueries) => [...prevQueries, ...newQueries]);
+          setPage((prevPage) => prevPage + 1);
+          setStatus(Status.RESOLVED);
+          if (newQueries.length < 12) {
+            setHasMore(false);
+          }
         }
       })
-      .catch(error => this.setState({ error, status: Status.REJECTED }))
+      .catch((error) => {
+        setError(error);
+        setStatus(Status.REJECTED);
+      });
   }
 
-  scrollPageToEnd = () => {
+  function onLoadMoreBtn() {
+    setTimeout(() => {
+      fetchQueries();
+    }, 500);
+    scrollPageToEnd();
+  }
+
+  function scrollPageToEnd() {
     setTimeout(() => {
       window.scrollBy({
         top: document.documentElement.scrollHeight,
-        behavior: 'smooth',
-      })
+        behavior: "smooth",
+      });
     }, 1000);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevQue = prevProps.queriesName;
-    const nextQue = this.props.queriesName;
-
-    if (prevQue !== nextQue) {
-      this.setState({ status: Status.PENDING, page: 1, queries: [], hasMore: true })
-      setTimeout(() => {
-        this.onFetchQuery()
-      }, 500);
-    }
+  function renderQueries() {
+    return (
+      <>
+        <ul className={s.ImageGallery}>
+          {queries.map(({ id, webformatURL, largeImageURL, tags }) => (
+            <ImageGalleryItem
+              key={id}
+              webformatURL={webformatURL}
+              largeImageURL={largeImageURL}
+              tags={tags}
+              handleImageClick={handleImageClick}
+            />
+          ))}
+        </ul>
+        {!hasMore && onErrorToastNoMore()}
+        {hasMore && (
+          <Button onClick={onLoadMoreBtn} aria-label="Load More" />
+        )}
+      </>
+    );
   }
 
-  render() {
-    const { queries, error, status, hasMore } = this.state
+  if (status === Status.IDLE) {
+    return <h1 className={s.queryText}>Enter your request</h1>;
+  }
 
-    if (status === Status.IDLE) {
-      return (
-        <>
-          <h1 className = {s.queryText}>Enter your request</h1>
-        </>
-      )
-    }
-    if(status === Status.PENDING){
-        return <GalleryLoader/>
-    }
+  if (status === Status.PENDING && queries.length === 0) {
+    return <GalleryLoader />;
+  }
 
-    if (status === Status.REJECTED) {
-      return (
-        <>
-          <h1>{error.message}</h1>
-        </>
-      )
-    }
+  if (status === Status.REJECTED) {
+    return <h1>{error.message}</h1>;
+  }
 
-    if (status === Status.RESOLVED) {
-      return (
-        <>
-          <ul className={s.ImageGallery}>
-            {queries.map(({ id, webformatURL, largeImageURL, tags }) => (
-              <ImageGalleryItem
-                key={id}
-                webformatURL={webformatURL}
-                largeImageURL={largeImageURL}
-                tags={tags}
-                handleImageClick={this.props.handleImageClick}
-              />
-            ))}
-          </ul>
-          {!hasMore && (
-          onErrorToastNoMore()
-            )}
-
-          {hasMore && (
-            <Button onClick={this.onLoadMoreBtn} aria-label="add" />
-          )}
-        </>
-      )
-    }
+  if (status === Status.RESOLVED) {
+    return renderQueries();
   }
 }
 
 ImageGallery.propTypes = {
-  queries: PropTypes.arrayOf(PropTypes.object),
+  queriesName: PropTypes.string,
   handleImageClick: PropTypes.func,
-  page: PropTypes.number,
-}
+};
 
 export default ImageGallery;
+
+
+
+
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import s from './ImageGallery.module.css';
+// import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+// import Button from '../Button/Button';
+// import {GalleryLoader } from '../Loader/Loader';
+// import { fetchQuery } from '../Api/Api';
+// import { onErrorToast} from '../ErorrTost/ErorrTost';
+// import { onErrorToastNoMore} from '../ErorrTost/ErorrTost';
+
+
+// const Status = {
+//   IDLE: 'idle',
+//   PENDING: 'pending',
+//   RESOLVED: 'resolved',
+//   REJECTED: 'rejected',
+// };
+
+// class ImageGallery extends Component {
+//   state = {
+//     queries: [],
+//     error: null,
+//     status: Status.IDLE,
+//     page: 1,
+//     hasMore: true
+//   }
+
+//   onLoadMoreBtn = (e) => {
+//     e.preventDefault();
+//     setTimeout(() => {
+//       this.onFetchQuery();
+//     }, 500);
+//     this.scrollPageToEnd()
+//   }
+
+//   onFetchQuery() {
+//     const searchQuery = this.props.queriesName;
+
+//     const { page } = this.state;
+//     fetchQuery(searchQuery, page)
+//       .then((queries) => {
+//         if (queries.hits.length === 0) {
+//           this.setState({ hasMore: false }, () => {
+//             onErrorToast();
+//           });
+//         } else {
+//           this.setState((prevState) => ({
+//             queries: [...prevState.queries, ...queries.hits],
+//             page: this.state.page + 1,
+//             status: Status.RESOLVED,
+//           }));
+//         }
+//       })
+//       .catch(error => this.setState({ error, status: Status.REJECTED }))
+//   }
+
+//   scrollPageToEnd = () => {
+//     setTimeout(() => {
+//       window.scrollBy({
+//         top: document.documentElement.scrollHeight,
+//         behavior: 'smooth',
+//       })
+//     }, 1000);
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const prevQue = prevProps.queriesName;
+//     const nextQue = this.props.queriesName;
+
+//     if (prevQue !== nextQue) {
+//       this.setState({ status: Status.PENDING, page: 1, queries: [], hasMore: true })
+//       setTimeout(() => {
+//         this.onFetchQuery()
+//       }, 500);
+//     }
+//   }
+
+//   render() {
+//     const { queries, error, status, hasMore } = this.state
+
+//     if (status === Status.IDLE) {
+//       return (
+//         <>
+//           <h1 className = {s.queryText}>Enter your request</h1>
+//         </>
+//       )
+//     }
+//     if(status === Status.PENDING){
+//         return <GalleryLoader/>
+//     }
+
+//     if (status === Status.REJECTED) {
+//       return (
+//         <>
+//           <h1>{error.message}</h1>
+//         </>
+//       )
+//     }
+
+//     if (status === Status.RESOLVED) {
+//       return (
+//         <>
+//           <ul className={s.ImageGallery}>
+//             {queries.map(({ id, webformatURL, largeImageURL, tags }) => (
+//               <ImageGalleryItem
+//                 key={id}
+//                 webformatURL={webformatURL}
+//                 largeImageURL={largeImageURL}
+//                 tags={tags}
+//                 handleImageClick={this.props.handleImageClick}
+//               />
+//             ))}
+//           </ul>
+//           {!hasMore && (
+//           onErrorToastNoMore()
+//             )}
+
+//           {hasMore && (
+//             <Button onClick={this.onLoadMoreBtn} aria-label="add" />
+//           )}
+//         </>
+//       )
+//     }
+//   }
+// }
+
+// ImageGallery.propTypes = {
+//   queries: PropTypes.arrayOf(PropTypes.object),
+//   handleImageClick: PropTypes.func,
+//   page: PropTypes.number,
+// }
+
+// export default ImageGallery;
 
 
 
