38fb8f985a333d6dd82595121974a5c096b6f6a9
viktoriazakatsyura <viktoriazakatsyura@gmail.com>
2023-04-09 14:41:30 +0300

    works

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,113 +1,106 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import s from "./ImageGallery.module.css";
-import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
-import Button from "../Button/Button";
-import { GalleryLoader } from "../Loader/Loader";
-import { fetchQuery } from "../Api/Api";
-import { onErrorToast } from "../ErorrTost/ErorrTost";
-import { onErrorToastNoMore } from "../ErorrTost/ErorrTost";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import s from './ImageGallery.module.css';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import Button from '../Button/Button';
+import { GalleryLoader } from '../Loader/Loader';
+import { fetchQuery } from '../Api/Api';
+import { onErrorToast } from '../ErorrTost/ErorrTost';
+import { onErrorToastNoMore } from '../ErorrTost/ErorrTost';
 
 const Status = {
-  IDLE: "idle",
-  PENDING: "pending",
-  RESOLVED: "resolved",
-  REJECTED: "rejected",
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
 };
 
-function ImageGallery({ queriesName, handleImageClick }) {
+const ImageGallery = ({ queriesName, handleImageClick }) => {
   const [queries, setQueries] = useState([]);
   const [error, setError] = useState(null);
   const [status, setStatus] = useState(Status.IDLE);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
 
+  const onLoadMoreBtn = (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+      onFetchQuery();
+    }, 500);
+    scrollPageToEnd();
+  };
+
+  const onFetchQuery = () => {
+    const searchQuery = queriesName;
+
+    fetchQuery(searchQuery, page)
+      .then((res) => {
+        const { hits } = res;
+
+        if (hits.length === 0) {
+          setHasMore(false);
+          onErrorToast();
+        } else {
+          setQueries((prevQueries) => [...prevQueries, ...hits]);
+          setPage((prevPage) => prevPage + 1);
+          setStatus(Status.RESOLVED);
+        }
+      })
+      .catch((err) => {
+        setError(err);
+        setStatus(Status.REJECTED);
+      });
+  };
+
+  const scrollPageToEnd = () => {
+    setTimeout(() => {
+      window.scrollBy({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
+      });
+    }, 1000);
+  };
+
   useEffect(() => {
-    if (queriesName !== "") {
+    if (queriesName) {
       setStatus(Status.PENDING);
       setPage(1);
       setQueries([]);
       setHasMore(true);
-      fetchQueries();
+
+      setTimeout(() => {
+        onFetchQuery();
+      }, 500);
     }
   }, [queriesName]);
 
-  function fetchQueries() {
-    fetchQuery(queriesName, page)
-      .then((response) => {
-        const newQueries = response.hits;
-        if (newQueries.length === 0) {
-          setHasMore(false);
-          onErrorToast();
-        } else {
-          setQueries((prevQueries) => [...prevQueries, ...newQueries]);
-          setPage((prevPage) => prevPage + 1);
-          setStatus(Status.RESOLVED);
-          if (newQueries.length < 12) {
-            setHasMore(false);
-          }
-        }
-      })
-      .catch((error) => {
-        setError(error);
-        setStatus(Status.REJECTED);
-      });
-  }
-
-  function onLoadMoreBtn() {
-    setTimeout(() => {
-      fetchQueries();
-    }, 500);
-    scrollPageToEnd();
-  }
-
-  function scrollPageToEnd() {
-    setTimeout(() => {
-      window.scrollBy({
-        top: document.documentElement.scrollHeight,
-        behavior: "smooth",
-      });
-    }, 1000);
-  }
-
-  function renderQueries() {
-    return (
-      <>
-        <ul className={s.ImageGallery}>
-          {queries.map(({ id, webformatURL, largeImageURL, tags }) => (
-            <ImageGalleryItem
-              key={id}
-              webformatURL={webformatURL}
-              largeImageURL={largeImageURL}
-              tags={tags}
-              handleImageClick={handleImageClick}
-            />
-          ))}
-        </ul>
-        {!hasMore && onErrorToastNoMore()}
-        {hasMore && (
-          <Button onClick={onLoadMoreBtn} aria-label="Load More" />
-        )}
-      </>
-    );
-  }
-
-  if (status === Status.IDLE) {
-    return <h1 className={s.queryText}>Enter your request</h1>;
-  }
-
-  if (status === Status.PENDING && queries.length === 0) {
-    return <GalleryLoader />;
-  }
-
-  if (status === Status.REJECTED) {
-    return <h1>{error.message}</h1>;
-  }
-
-  if (status === Status.RESOLVED) {
-    return renderQueries();
-  }
-}
+  return (
+    <>
+      {status === Status.IDLE && <h1 className={s.queryText}>Enter your request</h1>}
+      {status === Status.PENDING && <GalleryLoader />}
+      {status === Status.REJECTED && <h1>{error.message}</h1>}
+      {status === Status.RESOLVED && (
+        <>
+          <ul className={s.ImageGallery}>
+            {queries.map(({ id, webformatURL, largeImageURL, tags }) => (
+              <ImageGalleryItem
+                key={id}
+                webformatURL={webformatURL}
+                largeImageURL={largeImageURL}
+                tags={tags}
+                handleImageClick={handleImageClick}
+              />
+            ))}
+          </ul>
+          {!hasMore && onErrorToastNoMore()}
+          {hasMore && queries.length >= 12 && (
+            <Button onClick={onLoadMoreBtn} aria-label="add" />
+          )}
+        </>
+      )}
+    </>
+  );
+};
 
 ImageGallery.propTypes = {
   queriesName: PropTypes.string,
